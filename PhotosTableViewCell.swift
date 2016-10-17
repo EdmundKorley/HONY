9fb77c08509fa7282c24dc7fbc81886f32efa22b
Edmund Korley <korley.em@gmail.com>
2016-10-17 15:58:14 -0700

    Create TableViewCell and link to PhotosTableViewCell.swift

---
+++ PhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  
+//
+//  Created by Edmund Korley on 2016-10-17.
+//
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
